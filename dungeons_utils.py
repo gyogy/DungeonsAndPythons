de3868339f281396743a0ae494b93f5eaca19129
Niikcety <nikolaineshev1999@gmail.com>
2020-04-03 17:58:14 +0300

    Resolved conflicts in classes.py

--- dungeons_utils.py
+++ dungeons_utils.py
@@ -4,12 +4,15 @@ def load_map(map_path):
     dungeon_map = []
     dungeon_level = split_line(file.readline())
 
-    while(dungeon_level != split_line('--Treasures\n')):
-        dungeon_level.remove('\n')
+    while(dungeon_level != split_line('--Treasures\n') and dungeon_level != split_line('--Treasures')):
+        if(dungeon_level == []):
+            raise ValueError('Not a correct format for the txt file')
+        if('\n' in dungeon_level):
+            dungeon_level = dungeon_level[0:-1]
         dungeon_map.append(dungeon_level)
         dungeon_level = split_line(file.readline())
         
-
+    file.close()
     return dungeon_map        
 
 # Splits line from file to to list of chars 
@@ -19,14 +22,20 @@ def split_line(word):
 def load_treasures(map_path):
     file = open(map_path, "r")
     treasures = []
-    treasure = []
-    while(file.readline() != '--Treasures\n'):
-        pass
+    treasure = file.readline().split(', ')
+    while(treasure != ['--Treasures\n']):
+        #Reaching end of the file without Treasure line found
+        if treasure == ['']:
+            raise ValueError('Incorrect format of file(No --Treasures)')
+        treasure = file.readline().split(', ')
 
     treasure = file.readline().split(', ')
 
     while(treasure != ['--Enemies\n']):
+        if treasure == ['']:
+            raise ValueError('Incorrect format of file(No --Enemies)')
         treasures.append(treasure)
         treasure = file.readline().split(', ')
 
+    file.close()
     return treasures
